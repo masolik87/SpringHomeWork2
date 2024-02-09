638893b2ea885a24c9dfec4b706dc01cc4efef74
dimon <dimon20.07@mail.ru>
2024-02-09 17:06:43 +0300

    .\src\

---
+++ src/main/java/com/example/CRUD/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.CRUD.service;
+
+import com.example.CRUD.model.User;
+import com.example.CRUD.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public Object deleteById;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+}
